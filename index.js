13e7096409585a224e1521025f95d8e9af849e29
Max <max.lepan@laposte.net>
2021-03-08 11:51:21 +0100

    changed http.listen

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
 const app = require('express')();
-const http = require('http').createServer(app);
+const http = require('http').Server(app);
 const io = require('socket.io')(http);
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/index.html');
+  res.sendFile(__dirname + '/index.html');
 });
 
 io.on('connection', (socket) => {
-    socket.on('chat message', (msg) => {
-        io.emit('chat message', msg);
-    });
+  socket.on('chat message', msg => {
+    io.emit('chat message', msg);
+  });
 });
 
-http.listen(3000, () => {
-    console.log('listening on *:3000');
+http.listen(port, () => {
+  console.log(`Socket.IO server running at http://localhost:${port}/`);
 });
