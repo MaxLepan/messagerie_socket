35d3a546cb02e692b9fad09fcf68ac248293d2d7
Max <max.lepan@laposte.net>
2021-03-08 11:27:35 +0100

    added port const

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const app = require('express')();
 const http = require('http').createServer(app);
 const io = require('socket.io')(http);
+const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
