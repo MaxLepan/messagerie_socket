7d23a0620fd76da337e2d8165b80479911e1bc59
MaxLepan <max.lepan@laposte.net>
2021-03-11 11:47:28 +0100

    heroku test

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const io = require("socket.io")(http, {
         methods: ["GET", "POST"]
     }
 });
-const port = process.env.PORT || 3000;
+const port = process.env.PORT;
 
 var users = [];
 var messages = [];
