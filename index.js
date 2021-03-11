d5dacb7c391d9dccb9f45d61a70f81289e964a00
MaxLepan <max.lepan@laposte.net>
2021-03-11 11:45:40 +0100

    finding CORS bug

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 const express = require('express');
 const app = express();
 const http = require('http').createServer(app);
-const io = require('socket.io')(http);
+const io = require("socket.io")(http, {
+    cors: {
+        origin: "https://messageriesnap.herokuapp.com/",
+        methods: ["GET", "POST"]
+    }
+});
 const port = process.env.PORT || 3000;
 
 var users = [];
